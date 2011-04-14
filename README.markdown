74189788b4ff82841ef31f2d741e28ac3105e0bc
Mustang Server <ibbu26@yahoo.com>
2011-04-14 15:04:45 -0400

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Ruby on Rails Tutorial: demo application
+
+This is the demonstration application for [*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/) by [Michael Hart](http://michaelhartl.com).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '01c0fa73ee8e8ab294af6b26723e97f5a3ca3245b5307f095f01a218deda5b2000c868a9f5c2cc84cf5ebe144a5500dcda0c966ab470a581011270c034268e1d'
